fb5a426927aa15857c2c1c517c1a1aac24124627
Rafael Telles <rafael@telles.dev>
2019-11-12 00:37:55 -0300

    Speed up trie building and search for editted words instead of adding them to the trie

--- build_trie.py
+++ build_trie.py
@@ -6,8 +6,38 @@ import time
 LETTERS = b'\0abcdefghijklmnopqrstuvwxyz'
 
 class Trie(object):
-    def __init__(self, file_):
-        self.file_ = file_
+    def __init__(self):
+        self.buffer = bytearray(1024 * 1024 * 1024)
+        self.position = 0
+        self.length = 0
+    
+    def seek(self, position, whence=0):
+        if whence == 0:
+            self.position = position
+        elif whence == 1:
+            self.position += position
+        elif whence == 2:
+            self.position = self.length - position
+        else:
+            raise Error('Invalid whence')
+    
+    def tell(self):
+        return self.position
+    
+    def read(self, length):
+        value = self.buffer[self.position:self.position + length]
+        self.position += length
+        
+        return value
+
+    def write(self, value):
+        if self.position + len(value) > self.length:
+            self.length = self.position + len(value)
+        self.buffer[self.position:self.position + len(value)] = value
+        self.position += len(value)
+
+    def serialize(self):
+        return self.buffer[:self.length]
 
     @lru_cache(102400)
     def find_block(self, key):
@@ -17,113 +47,62 @@ class Trie(object):
         parent_block = self.find_block(key[:-1])
         char_index = LETTERS.index(key[-1])
         
-        self.file_.seek(parent_block + 4 * char_index)
+        self.seek(parent_block + 4 * char_index)
 
-        offset_pos = self.file_.tell()
-        offset = struct.unpack('i', self.file_.read(4))[0]
+        offset_pos = self.tell()
+        offset = struct.unpack('i', self.read(4))[0]
         if offset == -1:
             offset = self._create_block()
-            self.file_.seek(offset_pos)
-            self.file_.write(struct.pack('i', offset))
+            self.seek(offset_pos)
+            self.write(struct.pack('i', offset))
 
         return offset
 
-    def add(self, key, values):
+    def add(self, key, value):
         if not isinstance(key, bytes):
             key = bytes(key, 'ascii')
 
         pointer = self.find_block(key)
 
         while True:
-            self.file_.seek(pointer)
-            next_pointer = struct.unpack('i', self.file_.read(4))[0]
+            self.seek(pointer)
+            next_pointer = struct.unpack('i', self.read(4))[0]
 
             if next_pointer != -1:
                 pointer = next_pointer
             else:
                 break
 
-        for value in values:
-            if not isinstance(value, bytes):
-                value = bytes(value, 'ascii')
+        if not isinstance(value, int):
+            value = int(value)
 
-            value_offset = self._create_value(value)
-            self.file_.seek(pointer)
-            self.file_.write(struct.pack('i', value_offset))
-
-            pointer = value_offset
+        self.seek(pointer)
+        self.write(struct.pack('i', value))
 
     def _create_block(self):
-        pos = self.file_.tell()
+        pos = self.tell()
         
-        self.file_.seek(0, 2)
-        block_addr = self.file_.tell()
+        self.seek(0, 2)
+        block_addr = self.tell()
         
         size_of_block = (len(LETTERS) * 4)
-        self.file_.write(b'\xff' * size_of_block)
+        self.write(b'\xff' * size_of_block)
 
         return block_addr
-
-    def _create_value(self, value):
-        pos = self.file_.tell()
-        
-        self.file_.seek(0, 2)
-
-        block_addr = self.file_.tell()
-        self.file_.write(struct.pack('ii', -1, len(value)))
-        self.file_.write(value)
-
-        return block_addr
-
-
-
-def generate_variants(word):
-#    return [word]
-
-    # Source: https://norvig.com/spell-correct.html
-    "All edits that are one edit away from `word`."
-    letters    = 'abcdefghijklmnopqrstuvwxyz'
-    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
-    deletes    = [L + R[1:]               for L, R in splits if R]
-    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
-    inserts    = [L + c + R               for L, R in splits for c in letters]
-    return set(deletes + transposes + replaces + inserts + [word])
-
 
 if __name__ == '__main__':
-    trie_file = open("trie.data", 'w+b')
-    trie = Trie(trie_file)
+    trie = Trie()
     trie._create_block()
 
     print("Adding words...")
-    with open('words2.txt', 'r') as words_file:
+    with open('words.txt', 'r') as words_file:
         i = 0
-        j = 0
 
-        words = defaultdict(set)
-        def flush():
-            flush_words = len(words)
-            print(f'Flushing {flush_words} keys...')
-            start_t = time.time()
-
-            for key in sorted(words.keys()):
-                trie.add(key, words[key])
-            words.clear()
-            
-            took = time.time() - start_t
-            print(f'took {took} seconds. ({1000 * took / flush_words} ms per key.')
-
-            print(f'Total: {j} keys, {i} values')
         for word in words_file:
             word = word.strip()
-            
+            trie.add(word, i)            
             i += 1
-            for variant in generate_variants(word):
-                words[variant].add(word)
-                j += 1
 
-            if len(words) >= 50000:
-                flush()
-                
-        flush()
+    print("Write to file...")
+    with open("trie.data", 'w+b') as trie_file:
+        trie_file.write(trie.serialize())
--- search_trie.py
+++ search_trie.py
@@ -1,38 +1,80 @@
 import struct
 import sys
+import mmap
 
 LETTERS = b'\0abcdefghijklmnopqrstuvwxyz'
 
-def search(reader, key):
+def search(buffer, key):
+    if not isinstance(key, bytes):
+        key = bytes(key, 'ascii')
+    
     key += b'\0'
     i = 0
+    
+    offset = 0
     while i < len(key):
         char_index = LETTERS.index(key[i])
-        reader.seek(char_index * 4, 1)
 
-        offset = struct.unpack('i', reader.read(4))[0]
+        offset += char_index * 4
+        offset = struct.unpack('i', buffer[offset:offset + 4])[0]
         if offset == -1:
             return None
         
-        reader.seek(offset)
         i += 1
 
     values = []
     while offset != -1:
-        reader.seek(offset)
-        next_offset, value_length = struct.unpack('ii', reader.read(8))
-        value = reader.read(value_length)
+        next_offset, value_length = struct.unpack('ii', buffer[offset:offset + 8])
+        offset += 8
+        
+        value = buffer[offset:offset + value_length]
         values.append(value)
 
         offset = next_offset
     return values
 
 
+def edits(buffer, buffer_offset, prefix, word, results, depth):
+    letters_offsets = struct.unpack(f'{"i" * len(LETTERS)}', buffer[buffer_offset:buffer_offset + 4 * len(LETTERS)])
+    letter_to_offset = dict(filter(lambda pair: pair[1] >= 0, zip(LETTERS, letters_offsets)))
+
+    if len(word) == 0 and depth >= 0 and 0 in letter_to_offset:
+        results[prefix] = letter_to_offset[0]
+
+    if depth >= 1:
+        # deletion
+        edits(buffer, buffer_offset, prefix, word[1:] if len(word) > 0 else b'', results, depth - 1)
+
+        for letter, offset in letter_to_offset.items():
+            if letter == 0: continue
+
+            # insertion
+            edits(buffer, offset, prefix + bytes([letter]), word, results, depth - 1)
+
+            # substitution
+            edits(buffer, offset, prefix + bytes([letter]), word[1:], results, depth - 1)
+
+        # transposition
+        edits(buffer, buffer_offset, prefix, word[1:2] + word[0:1] + word[2:], results, depth - 1)
+
+
+    if len(word) >= 1 and word[0] in letter_to_offset:
+        edits(buffer, letter_to_offset[word[0]], prefix + word[0:1], word[1:], results, depth)
+
+def search(buffer, word):
+    if not isinstance(word, bytes):
+        word = bytes(word, 'ascii')
+    
+    results = {}
+    edits(buffer, 0, b'', word, results, 2)
+
+    return results
+
 if __name__ == '__main__':
-    reader = open('trie.data', 'rb')
     word = sys.argv[1]
-    key = bytes(word, 'ascii')
+    
+    with open('trie.data', 'r+') as trie_file:
+        buffer = mmap.mmap(trie_file.fileno(), 0)
 
-    value = search(reader, key)
-
-    print(value)
+        results = search(buffer, word)
+        print(results)
